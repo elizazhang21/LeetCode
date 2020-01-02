8b9b24f0839055b57db9f9f29629110a99571e50
ELIZABETH ZHANG <elizabeth.zhang@nyu.edu>
2020-01-02 18:49:10 +0800

    Add files via upload

---
+++ # 234. Palindrome Linked List.py
@@ -0,0 +1,29 @@
+# 234. Palindrome Linked List
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        fast = slow = head
+        # find the mid node
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+        # reverse the second half
+        node = None
+        while slow:
+            nxt = slow.next
+            slow.next = node
+            node = slow
+            slow = nxt
+        # compare the first and second half nodes
+        while node: # while node and head:
+            if node.val != head.val:
+                return False
+            node = node.next
+            head = head.next
+        return True
